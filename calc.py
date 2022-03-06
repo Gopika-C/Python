9ae69f8cb8e4859eea99bc952eb8b799b824f159
Gopika-C <95700408+Gopika-C@users.noreply.github.com>
2022-03-06 09:34:50 +0530

    Add files via upload

--- calc.py
+++ calc.py
@@ -1,31 +1,19 @@
-def add(x,y):
-	return x+y
-def sub(x,y):
-	return x-y
-def mul(x,y):
-	return x*y
-def div(x,y):
-	return x/y
-def pow(x,y):
-	return x**y
-def mod(x,y):
-	return x%y
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+op=input(" Enter operator +,-,*,/,^ : \n")
+if(op=="+"):
+	print("sum is",a+b)
+elif(op=="-"):
+	print("difference is",a-b)
+elif(op=="*"):
+	print("product is",a*b)
+elif(op=="/"):
+	print("quotient is",a//b)
+elif(op=="%"):
+	print("remainder is",a%b)
+elif(op=="^"):
+	print("power is",a**b)
 
-print("Select Operations\n1-Add\n2-Subtract\n3-Multiply\n4-Division\n5-Power\n6-Modulus")
-choice=input("Enter choice(1/2/3/4/5/6) : ")
-num1=int(input("Enter first number : "))
-num2=int(input("Enter second number : "))
-if choice=='1':
-	print(num1,"+",num2,"=",add(num1,num2))
-elif choice=='2':
-	print(num1,"-",num2,"=",sub(num1,num2))
-elif choice=='3':
-	print(num1,"*",num2,"=",mul(num1,num2))
-elif choice=='4':
-	print(num1,"/",num2,"=",div(num1,num2))
-elif choice=='5':
-	print(num1,"^",num2,"=",pow(num1,num2))
-elif choice=='6':
-	print(num1,"%",num2,"=",mod(num1,num2))
 else:
-	print("Invalid input")
+	print("Not recognised operator")
+
